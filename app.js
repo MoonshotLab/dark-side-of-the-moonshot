44db62878ac20ad49e51d99c3a9831bb149b5ee1
Preston Richey <prichey@barkleyus.com>
2016-10-19 15:38:05 -0500

    Init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const appPort = 11235;
+
+// legacy blog post, redirect to tumblr
+app.get('/post/*', function(req, res) {
+  let tumblrBaseUrl = "https://barkleymoonshot.tumblr.com";
+  let newUrl = tumblrBaseUrl + req.originalUrl;
+
+  res.redirect(newUrl);
+});
+
+// anything else, redirect to medium
+app.get('*', function(req, res) {
+  let mediumUrl = "https://medium.com/moonshotlab";
+
+  res.redirect(mediumUrl);
+});
+
+app.listen(appPort, function() {
+  console.log(`Listening on port ${appPort}.`);
+});
